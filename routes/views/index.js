c9611c3ce0d5a2439bf7b6ba0524bed3c6964ec9
David Rynearson <rynearson.david@gmail.com>
2017-06-10 14:38:06 -0700

    Fixing pagination issue

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,39 +8,24 @@ exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	req.params "page 2"
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
-	locals.section = req.url;
+	locals.section = '/blog';
 	locals.validationErrors = {};
 	// Load the current post
 	view.on('init', function (next) {
-		Post.model.findOne()
+		Post.paginate({
+			page: req.query.page || 1,
+			perPage: 2,
+			maxPages: 10
+		})
+			.where('state', 'published')
+			.sort('-publishedDate')
 			.exec(function(err, results) {
-				locals.data = results;
-				if(locals.section == '/blog' ) {
-					Post.paginate({
-						page: req.query.page || 1,
-						perPage: 2,
-						maxPages: 10
-					})
-						.where('state', 'published')
-						.sort('-publishedDate')
-						.exec(function(err, results) {
-							locals.posts = results;
-							next(err);
-						});
-				} else {
-					Post.model.find()
-						.where('state', 'published')
-						.sort('-publishedDate')
-						.limit(5)
-						.exec(function(err, results) {
-							locals.posts = results;
-							next();
-						});
-				}
+				locals.posts = results;
+				next(err);
 			});
+		next();
 	});
 	view.on('init', function (next) {
 		Video.model.findOne()
