437851ebdcd99ddb52ec08ca1cd440e587119060
David Rynearson <rynearson.david@gmail.com>
2017-08-06 13:16:26 -0700

    Adding in blog header for top part

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -1,5 +1,7 @@
 var keystone = require('keystone');
 var Index = keystone.list('Index');
+var Post = keystone.list('Post');
+
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
@@ -10,13 +12,23 @@ exports = module.exports = function (req, res) {
 	locals.validationErrors = {};
 	// Load the current post
 	view.on('init', function (next) {
+		Post.paginate({
+			page: req.query.page || 1,
+			perPage: 2,
+			maxPages: 10
+		})
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.exec(function(err, results) {
+				locals.posts = results;
+			});
+
 		Index.model.findOne()
 			.exec(function(err, results) {
 				locals.data = results;
 					var p = keystone.list('Post').model.findById(req.params.post);
 					p.exec(function(err, result){
 						locals.post = result;
-						console.log(result);
 						next(err);
 					});
 			});
